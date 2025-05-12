6955c6aa833d552b815ff6ae50dbff19630a86e6
Anne Beumer <anne.beumer@student.uva.nl>
2025-05-12 17:53:32 +0200

    Versie waarbij Main hem nu automatisch opent

--- Test_NICU/src/main/java/org/example/Main.java
+++ Test_NICU/src/main/java/org/example/Main.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @SpringBootApplication
@@ -16,10 +20,25 @@ public class Main {
         //String encodedPassword = encoder.encode(rawPassword);
         //System.out.println(encodedPassword);
 
-        DatabaseQuery.select();
+        //DatabaseQuery.select();
         SpringApplication.run(Main.class, args);
+        openBrowser("http://localhost:8080/login.html");
         //DatabaseQuery.insert();
         //DatabaseQuery.select();
     }
+
+    private static void openBrowser(String url) {
+        try {
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().browse(new URI(url));
+            } else {
+                // Fallback voor macOS
+                Runtime.getRuntime().exec(new String[]{"open", url});
+            }
+        } catch (Exception e) {
+            System.err.println("Kon browser niet openen: " + e.getMessage());
+        }
+    }
+
 
 }
