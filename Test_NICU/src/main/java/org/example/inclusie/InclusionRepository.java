50d68eb3a4b990ed46922386b550481a232a043d
joostgoddijn <joostgoddijn@casema.nl>
2025-05-20 21:18:29 +0200

    Twee grafieken toegevoegd aan dashboard.

--- Test_NICU/src/main/java/org/example/inclusie/InclusionRepository.java
+++ Test_NICU/src/main/java/org/example/inclusie/InclusionRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface InclusionRepository extends JpaRepository<Inclusion, InclusionId>{
+public interface InclusionRepository extends JpaRepository<Inclusion, InclusionId> {
 
     @Query("SELECT i FROM Inclusion i WHERE i.naam_studie = :naam_studie AND i.naam_centrum = :naam_centrum ORDER BY i.datum DESC")
     List<Inclusion> findAllByStudyAndCenterOrderByDatumDesc(@Param("naam_studie") String naamStudie, @Param("naam_centrum") String naamCentrum);
